6e3996debe3a9e390166eca2147fe4030e36843e
Dave Kelly <dave.a.kelly@gmail.com>
2017-12-25 12:53:53 +0000

    Initial commit

---
+++ HexCalculator1/HexConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HexCalculator1
+{
+   public static class HexConverter
+   {
+      public static string ConvertDecToHex (string input)
+      {
+         int value = 0;
+
+         int.TryParse (input, out value);
+
+         return value.ToString ("X");
+      }
+   }
+}
